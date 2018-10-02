d5b95bd09d871782fecabbfb165ebafea53d2752
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    op3: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- device.mk
+++ device.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # Thermal
 PRODUCT_PACKAGES += \
